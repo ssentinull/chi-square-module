aa1f6b9966fd2580ad8e3f1480dca4be50ce7d1a
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-07 11:10:10 +0700

    refactor: modularize function that generate tokens

--- src/index.js
+++ src/index.js
@@ -9,22 +9,28 @@ const DATA_DIR = "./data/1000-sample-data.csv";
 const readCsv = async fileDir => {
   const csvText = fs.readFileSync(fileDir, "utf-8");
   const formattedCsv = await neatCsv(csvText);
+
   return formattedCsv;
 };
 
-(async () => {
+const generateTokens = abstract => {
   const tokenizer = new WordTokenizer();
+
+  const abstractLowercase = lowerCase(abstract);
+  const abstractTokens = tokenizer.tokenize(abstractLowercase);
+  const tokensStopwordRemoved = sw.removeStopwords(abstractTokens, sw.id);
+  const tokensStemmed = tokensStopwordRemoved.map(word => StemmerId.stem(word));
+
+  return tokensStemmed;
+};
+
+(async () => {
   const csv = await readCsv(DATA_DIR);
 
   for (const row of csv) {
     const { ARTICLE_ABSTRACT } = row;
-    const abstractToLowercase = lowerCase(ARTICLE_ABSTRACT);
-    const abstractTokens = tokenizer.tokenize(abstractToLowercase);
-    const tokensStopwordRemoved = sw.removeStopwords(abstractTokens, sw.id);
-    const tokensStemmed = tokensStopwordRemoved.map(word =>
-      StemmerId.stem(word)
-    );
+    const tokens = generateTokens(ARTICLE_ABSTRACT);
 
-    row.TOKENS = tokensStemmed;
+    row.TOKENS = tokens;
   }
 })();

7d4a1d9e464274862f92414419cfcb20bd482a6e
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-11 19:57:48 +0700

    feat: read & write to .json file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const sw = require("stopword");
 const { lowerCase } = require("lower-case");
 const { WordTokenizer, StemmerId } = require("natural");
 
-const DATA_DIR = "./data/1000-sample-data.csv";
+const DATASET_DIR = "./data/1000-sample-data.csv";
+const JSON_SAVE_DIR = "./data/csv.json";
 
 const readCsv = async fileDir => {
   const csvText = fs.readFileSync(fileDir, "utf-8");
@@ -12,6 +13,21 @@ const readCsv = async fileDir => {
 
   return formattedCsv;
 };
+
+const readJson = (file, encoding) => {
+  const csvJsonContents = fs.readFileSync(file, encoding);
+  const csvJson = JSON.parse(csvJsonContents);
+
+  return csvJson;
+};
+
+const replacer = (key, value) => (key === "JOURNAL_ID" ? +value : value);
+
+const writeJson = (file, variable, replacer, spaces) =>
+  fs.writeFileSync(file, JSON.stringify(variable, replacer, spaces), err => {
+    if (err) throw err;
+    console.log("complete writing as .json file");
+  });
 
 const generateTokens = abstract => {
   const tokenizer = new WordTokenizer();
@@ -25,12 +41,16 @@ const generateTokens = abstract => {
 };
 
 (async () => {
-  const csv = await readCsv(DATA_DIR);
+  const csvDataFile = await readCsv(DATASET_DIR);
 
-  for (const row of csv) {
+  for (const row of csvDataFile) {
     const { ARTICLE_ABSTRACT } = row;
     const tokens = generateTokens(ARTICLE_ABSTRACT);
 
     row.TOKENS = tokens;
   }
+
+  writeJson(JSON_SAVE_DIR, csvDataFile, replacer, 2);
+
+  const csvDataJson = readJson("csv.json", "utf-8");
 })();

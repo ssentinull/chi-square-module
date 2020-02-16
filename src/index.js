da1aceb092d282c08905bfa3848e80ff58f6fb85
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-16 12:34:54 +0700

    refactor: separate chiSquare calcutaion to chiSquare util

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {
 } = require("./utils/text-processing.util");
 const { readCsv, readJson, writeJson } = require("./utils/io.util");
 const {
+  calculateChiSquareValues,
   createTokenList,
   groupTokenListWithJsonData,
   sortChiSquareValueDescendingly
@@ -11,70 +12,6 @@ const {
 
 const DATASET_DIR = "./data/dataset-sample.csv";
 const JSON_SAVE_DIR = "./data/csv.json";
-
-const chiSquareFeatureSelection = (tokenList, jsonData) => {
-  let [aValue, bValue, cValue, dValue] = [0, 0, 0, 0];
-
-  // compute the A, B, C, D, and X_SQUARED values
-  // go over every single tokens that's been spread out
-  for (const tokenListRow of tokenList) {
-    const {
-      JOURNAL_ID: JOURNAL_ID_TOKEN_LIST,
-      TOKEN: TOKEN_TOKEN_LIST
-    } = tokenListRow;
-
-    // check for the existance of the tokens in each articles
-    for (const jsonDataRow of jsonData) {
-      const {
-        JOURNAL_ID: JOURNAL_ID_JSON_DATA,
-        TOKENS_DUPLICATE_REMOVED: TOKEN_JSON_DATA
-      } = jsonDataRow;
-
-      if (
-        JOURNAL_ID_TOKEN_LIST === JOURNAL_ID_JSON_DATA &&
-        TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
-      ) {
-        aValue += 1;
-      }
-
-      if (
-        JOURNAL_ID_TOKEN_LIST !== JOURNAL_ID_JSON_DATA &&
-        TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
-      ) {
-        bValue += 1;
-      }
-
-      if (
-        JOURNAL_ID_TOKEN_LIST === JOURNAL_ID_JSON_DATA &&
-        !TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
-      ) {
-        cValue += 1;
-      }
-
-      if (
-        JOURNAL_ID_TOKEN_LIST !== JOURNAL_ID_JSON_DATA &&
-        !TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
-      ) {
-        dValue += 1;
-      }
-    }
-
-    const chiSquare =
-      (aValue * dValue - bValue * cValue) ** 2 /
-      ((aValue + bValue) * (cValue + dValue));
-
-    [
-      tokenListRow.A_VALUE,
-      tokenListRow.B_VALUE,
-      tokenListRow.C_VALUE,
-      tokenListRow.D_VALUE,
-      tokenListRow.CHI_SQUARED
-    ] = [aValue, bValue, cValue, dValue, chiSquare];
-    [aValue, bValue, cValue, dValue] = [0, 0, 0, 0];
-  }
-
-  return jsonData;
-};
 
 (async () => {
   const csvDataFile = await readCsv(DATASET_DIR);
@@ -94,13 +31,12 @@ const chiSquareFeatureSelection = (tokenList, jsonData) => {
 
   const csvDataJson = readJson(JSON_SAVE_DIR);
   const tokenList = createTokenList(csvDataJson);
-  const featureVectors = chiSquareFeatureSelection(tokenList, csvDataJson);
+  const featureVectors = [];
 
-  groupTokenListWithJsonData(tokenList, featureVectors);
+  for (const tokenListRow of tokenList) {
+    const chiSquareValues = calculateChiSquareValues(tokenListRow, csvDataJson);
+    const featureVector = { ...tokenListRow, ...chiSquareValues };
 
-  for (const row of featureVectors) {
-    const { TOKENS_SCORES } = row;
-
-    sortChiSquareValueDescendingly(TOKENS_SCORES);
+    featureVectors.push(featureVector);
   }
 })();
--- src/utils/chi-square.util.js
+++ src/utils/chi-square.util.js
@@ -1,3 +1,64 @@
+const calculateChiSquareValues = (tokenListRow, jsonData) => {
+  let [aValue, bValue, cValue, dValue] = [0, 0, 0, 0];
+
+  // compute the A, B, C, D, and X_SQUARED values
+  // go over every single tokens that's been spread out
+  const {
+    JOURNAL_ID: JOURNAL_ID_TOKEN_LIST,
+    TOKEN: TOKEN_TOKEN_LIST
+  } = tokenListRow;
+
+  // check for the existance of the tokens in each articles
+  for (const jsonDataRow of jsonData) {
+    const {
+      JOURNAL_ID: JOURNAL_ID_JSON_DATA,
+      TOKENS_DUPLICATE_REMOVED: TOKEN_JSON_DATA
+    } = jsonDataRow;
+
+    if (
+      JOURNAL_ID_TOKEN_LIST === JOURNAL_ID_JSON_DATA &&
+      TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
+    ) {
+      aValue += 1;
+    }
+
+    if (
+      JOURNAL_ID_TOKEN_LIST !== JOURNAL_ID_JSON_DATA &&
+      TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
+    ) {
+      bValue += 1;
+    }
+
+    if (
+      JOURNAL_ID_TOKEN_LIST === JOURNAL_ID_JSON_DATA &&
+      !TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
+    ) {
+      cValue += 1;
+    }
+
+    if (
+      JOURNAL_ID_TOKEN_LIST !== JOURNAL_ID_JSON_DATA &&
+      !TOKEN_JSON_DATA.includes(TOKEN_TOKEN_LIST)
+    ) {
+      dValue += 1;
+    }
+  }
+
+  const chiSquare =
+    (aValue * dValue - bValue * cValue) ** 2 /
+    ((aValue + bValue) * (cValue + dValue));
+
+  const chiSquareValues = {
+    A_VALUE: aValue,
+    B_VALUE: bValue,
+    C_VALUE: cValue,
+    D_VALUE: dValue,
+    CHI_SQUARED: chiSquare
+  };
+
+  return chiSquareValues;
+};
+
 // spread the tokens of the articles into a single layer array of objects
 const createTokenList = jsonData => {
   const tokenList = [];
@@ -37,6 +98,7 @@ const sortChiSquareValueDescendingly = list =>
   list.sort((a, b) => b.CHI_SQUARED - a.CHI_SQUARED);
 
 module.exports = {
+  calculateChiSquareValues,
   createTokenList,
   groupTokenListWithJsonData,
   sortChiSquareValueDescendingly

851ad1d8b967f4470c7d45476b69f658c6ad0b4b
IbnuAhsani <ibnu.muhari@gmail.com>
2020-05-05 04:47:45 +0700

    feat: save fvs & tf-idf scores for 500 features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const { calculateTfIdfScores } = require("./utils/tf-idf.util");
-const { writeCsv } = require("./utils/io.util");
 const { tokensGenerator } = require("./modules/tokensGenerator");
+const { writeCsv } = require("./utils/io.util");
 const {
   featureVectorsGenerator,
 } = require("./modules/featureVectrosGenerator");
 
-("./data/output/fv-tokens-by-journal.json");
-const TF_IDF_SCORES_200_SAVE_PATH =
-  "./data/output/tf-idf/fv-tf-idf-scores-200.csv";
-const TF_IDF_SCORES_100_SAVE_PATH =
-  "./data/output/tf-idf/fv-tf-idf-scores-100.csv";
 const TF_IDF_SCORES_50_SAVE_PATH =
   "./data/output/tf-idf/fv-tf-idf-scores-50.csv";
+const TF_IDF_SCORES_100_SAVE_PATH =
+  "./data/output/tf-idf/fv-tf-idf-scores-100.csv";
+const TF_IDF_SCORES_200_SAVE_PATH =
+  "./data/output/tf-idf/fv-tf-idf-scores-200.csv";
+const TF_IDF_SCORES_500_SAVE_PATH =
+  "./data/output/tf-idf/fv-tf-idf-scores-500.csv";
 
 (async () => {
   const processBegin = Date.now();
@@ -27,14 +28,15 @@ const TF_IDF_SCORES_50_SAVE_PATH =
     TOP_50_FEATURE_VECTORS: uniqueTop50MFeatureVectors,
     TOP_100_FEATURE_VECTORS: uniqueTop100MFeatureVectors,
     TOP_200_FEATURE_VECTORS: uniqueTop200MFeatureVectors,
+    TOP_500_FEATURE_VECTORS: uniqueTop500MFeatureVectors,
   } = featureVectorsGeneratorResult;
 
   //////////////////////////////
 
   console.time("calculate-tfidf-score");
 
-  const featureVectorsTop200MTfidfScores = calculateTfIdfScores(
-    uniqueTop200MFeatureVectors,
+  const featureVectorsTop50MTfidfScores = calculateTfIdfScores(
+    uniqueTop50MFeatureVectors,
     jsonData
   );
 
@@ -42,8 +44,14 @@ const TF_IDF_SCORES_50_SAVE_PATH =
     uniqueTop100MFeatureVectors,
     jsonData
   );
-  const featureVectorsTop50MTfidfScores = calculateTfIdfScores(
-    uniqueTop50MFeatureVectors,
+
+  const featureVectorsTop200MTfidfScores = calculateTfIdfScores(
+    uniqueTop200MFeatureVectors,
+    jsonData
+  );
+
+  const featureVectorsTop500MTfidfScores = calculateTfIdfScores(
+    uniqueTop500MFeatureVectors,
     jsonData
   );
 
@@ -55,9 +63,10 @@ const TF_IDF_SCORES_50_SAVE_PATH =
 
   console.time("save-tfidf-scores-as-csv");
 
-  writeCsv(TF_IDF_SCORES_200_SAVE_PATH, featureVectorsTop200MTfidfScores);
-  writeCsv(TF_IDF_SCORES_100_SAVE_PATH, featureVectorsTop100MTfidfScores);
   writeCsv(TF_IDF_SCORES_50_SAVE_PATH, featureVectorsTop50MTfidfScores);
+  writeCsv(TF_IDF_SCORES_100_SAVE_PATH, featureVectorsTop100MTfidfScores);
+  writeCsv(TF_IDF_SCORES_200_SAVE_PATH, featureVectorsTop200MTfidfScores);
+  writeCsv(TF_IDF_SCORES_500_SAVE_PATH, featureVectorsTop500MTfidfScores);
 
   console.log("done saving tf-idf scores as .csv");
   console.timeEnd("save-tfidf-scores-as-csv");

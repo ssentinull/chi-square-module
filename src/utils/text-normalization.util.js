9fd80f5f4eceb1c3e8369f7f3cad678ade51219b
IbnuAhsani <ibnu.muhari@gmail.com>
2020-05-05 04:49:17 +0700

    feat: filter tokens with >= 3 letters in text-norm util

--- src/utils/text-normalization.util.js
+++ src/utils/text-normalization.util.js
@@ -2,7 +2,7 @@ const sw = require("stopword");
 const { lowerCase } = require("lower-case");
 const { WordTokenizer, StemmerId } = require("natural");
 
-const generateTokens = abstract => {
+const generateTokens = (abstract) => {
   const tokenizer = new WordTokenizer();
 
   const abstractLowercase = lowerCase(abstract);
@@ -10,12 +10,12 @@ const generateTokens = abstract => {
   const abstractTokens = tokenizer.tokenize(abstractDigitsRemoved);
   const tokensStopwordRemoved = sw.removeStopwords(abstractTokens, sw.id);
   const tokensStemmed = tokensStopwordRemoved
-    .filter(word => word.length >= 2)
-    .map(word => StemmerId.stem(word));
+    .filter((word) => word.length >= 3)
+    .map((word) => StemmerId.stem(word));
 
   return tokensStemmed;
 };
 
-const removeDuplicateTokens = tokens => [...new Set(tokens)];
+const removeDuplicateTokens = (tokens) => [...new Set(tokens)];
 
 module.exports = { generateTokens, removeDuplicateTokens };

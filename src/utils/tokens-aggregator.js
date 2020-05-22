07e4a7d81ea5c2bc8e96fb2547f7e9d26fe5482a
IbnuAhsani <ibnu.muhari@gmail.com>
2020-05-22 13:24:29 +0700

    feat: journal abstract tokens aggregator

---
+++ src/utils/tokens-aggregator.js
@@ -0,0 +1,21 @@
+const { readJson, writeJson } = require("./io.util");
+
+const DATASET_PATH = "./data/output/dataset-sample.json";
+const OUTPUT_PATH = "./src/utils/tokens-to-regex.json";
+
+(async () => {
+  const jsonDatas = await readJson(DATASET_PATH);
+  const regexTokens = {};
+
+  for (const jsonData of jsonDatas) {
+    const { TOKENS_DUPLICATE_REMOVED: tokens } = jsonData;
+
+    for (const token of tokens) {
+      if (!(token in regexTokens)) regexTokens[token] = token;
+    }
+  }
+
+  console.log("iran");
+
+  writeJson(OUTPUT_PATH, regexTokens);
+})();
